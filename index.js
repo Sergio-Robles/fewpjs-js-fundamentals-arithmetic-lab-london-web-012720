3bbb88a8aed9299ce6c294de10f3389a3aab45c7
Sergio Robles <sroblesc@gmail.com>
2020-05-01 16:17:18 +0100

    Saved

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+const num1 = Number.parseInt(num1)
+const num2 = Number.parseInt(num2)
+
+const multiply = (num1 * num2)
+
+const random = Math.random()
+
+
+const max = Math.max(1, 3, 20)
+
